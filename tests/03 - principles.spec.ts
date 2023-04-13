83f5c0cbb315ccb086c6498351445a59fc8010b2
André Cordazzo Brunelli <andre.cordazzo-brunelli@sogeti.no>
2023-04-13 12:42:59 +0200

    added extended expect, web-first assertion

--- tests/03 - principles.spec.ts
+++ tests/03 - principles.spec.ts
@@ -1,21 +1,37 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 // tests in file run in parallel
-test.describe.configure({ mode: 'parallel' });
+test.describe.configure({ mode: "parallel" });
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
+});
+
+test("Wrong expect", async ({ page }) => {
+  await page.goto("http://advantageonlineshopping.com/#/");
+  expect(
+    await page
+      .getByRole("link", { name: "TabletsCategory", exact: true })
+      .isVisible()
+  ).toBe(true);
+});
+
+test("Right expect (web-first assertions)", async ({ page }) => {
+  await page.goto("http://advantageonlineshopping.com/#/");
+  await expect(
+    page.getByRole("link", { name: "TabletsCategory", exact: true })
+  ).toBeVisible();
 });
